53e7093271bcd631451747f8fe351782beb81bed
Filip Tatarnik <ftatarnik@gmail.com>
2026-01-06 16:12:11 -0600

    fresh game save

---
+++ src/Components/Login.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import './Login.css';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Hello</h1>
+         );
+    }
+}
+ 
+export default Login;
